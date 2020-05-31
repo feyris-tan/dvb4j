584b5d7ed0e470653e9e24e8625cf0d1bafb1d64
Feyris-Tan <ft@feyris-tan.lebtimnetz.de>
2020-05-31 22:32:58 +0200

    Discard some invalid SDT

--- src/main/java/moe/yo3explorer/dvb4j/decoders/SDTDecoder.java
+++ src/main/java/moe/yo3explorer/dvb4j/decoders/SDTDecoder.java
@@ -51,7 +51,7 @@ public class SDTDecoder implements PSIDecoder {
                 int descriptorLength = payload.get() & 0xff;
                 descriptorsLength -= 2;
 
-                if (descriptorId == 0x61 || descriptorId == 0x40)
+                if (descriptorId == 0x61 || descriptorId == 0x40 || descriptorId == 0x63)
                 {
                     //Laut ETSI 300468 Seite 39 kommen die nicht in SDT vor
                     return;
